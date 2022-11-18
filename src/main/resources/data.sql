058546efc9674a7bbb93c97e5b78365840a4904c
Cris Konert <criskonert@gmail.com>
2022-11-18 15:24:43 +0100

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO authors (id, lastname, firstname) VALUES (1, 'Tolkien', 'JRR');
+INSERT INTO authors (id, lastname, firstname) VALUES (2, 'Martin', 'George RR');
+INSERT INTO authors (id, lastname, firstname) VALUES (3, 'Herbert', 'Frank');
+INSERT INTO authors (id, lastname, firstname) VALUES (4, 'Clavell', 'James');
+INSERT INTO authors (id, lastname, firstname) VALUES (5, 'Dahl', 'Roald');
+INSERT INTO books (id, title, author, isbn) VALUES (1, 'A Game of Thrones', 2, '0553808044');
+INSERT INTO books (id, title, author, isbn) VALUES (2, 'The Lord of the Rings', 1, '0063274736');
+INSERT INTO books (id, title, author, isbn) VALUES (3, 'Shogun', 4, '1982603844');
+INSERT INTO books (id, title, author, isbn) VALUES (4, 'Dune', 3, ' 059309932X');
+INSERT INTO books (id, title, author, isbn) VALUES (5, 'The BFG', 5, '0141371145');
+
