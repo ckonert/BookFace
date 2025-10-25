be0ecb01e5751a39358b5d69e972fea607b79897
Cris Konert <20204082+ckonert@users.noreply.github.com>
2025-10-25 16:19:30 +0200

    fixed cucumber error

--- src/main/java/nl/testwerk/bookface/config/WebServiceConfig.java
+++ src/main/java/nl/testwerk/bookface/config/WebServiceConfig.java
@@ -5,7 +5,11 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.server.endpoint.adapter.DefaultMethodEndpointAdapter;
+import org.springframework.ws.server.endpoint.adapter.method.MarshallingPayloadMethodProcessor;
+import org.springframework.ws.soap.server.SoapMessageDispatcher;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
@@ -20,14 +24,14 @@ public class WebServiceConfig {
         MessageDispatcherServlet servlet = new MessageDispatcherServlet();
         servlet.setApplicationContext(context);
         servlet.setTransformWsdlLocations(true);
-        return new ServletRegistrationBean<>(servlet, "/ws/*");
+        return new ServletRegistrationBean<>(servlet, "/allbooks/*");
     }
 
     @Bean(name = "books")
     public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema booksSchema) {
         DefaultWsdl11Definition wsdl11Definition = new DefaultWsdl11Definition();
         wsdl11Definition.setPortTypeName("BooksPort");
-        wsdl11Definition.setLocationUri("/ws");
+        wsdl11Definition.setLocationUri("/allbooks");
         wsdl11Definition.setTargetNamespace("http://testwerk.nl/bookface/soap");
         wsdl11Definition.setSchema(booksSchema);
         return wsdl11Definition;
@@ -37,4 +41,12 @@ public class WebServiceConfig {
     public XsdSchema booksSchema() {
         return new SimpleXsdSchema(new ClassPathResource("books.xsd"));
     }
+
+    @Bean
+    public Jaxb2Marshaller marshaller() {
+        Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+        marshaller.setContextPath("nl.testwerk.bookface.soap");
+        return marshaller;
+    }
+
 }
--- src/main/java/nl/testwerk/bookface/soap/BookEndpoint.java
+++ src/main/java/nl/testwerk/bookface/soap/BookEndpoint.java
@@ -1,14 +1,10 @@
 package nl.testwerk.bookface.soap;
 
-import nl.testwerk.bookface.model.Book;
 import nl.testwerk.bookface.repository.BookRepository;
-import nl.testwerk.bookface.soap.gen.*;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
-
-import java.util.List;
 
 @Endpoint
 public class BookEndpoint {
@@ -24,18 +20,17 @@ public class BookEndpoint {
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getAllBooksRequest")
     @ResponsePayload
     public GetAllBooksResponse getAllBooks(@RequestPayload GetAllBooksRequest request) {
-        GetAllBooksResponse response = new GetAllBooksResponse();
+        var response = new GetAllBooksResponse();
 
-        List<Book> books = bookRepository.findAll();
-        for (Book b : books) {
-            nl.testwerk.bookface.soap.gen.Book bookSoap = new nl.testwerk.bookface.soap.gen.Book();
+        bookRepository.findAll().forEach(b -> {
+            var bookSoap = new nl.testwerk.bookface.soap.Book();
             bookSoap.setId(b.getId());
             bookSoap.setTitle(b.getTitle());
             bookSoap.setIsbn13(b.getIsbn13());
             bookSoap.setAuthorId(b.getAuthorId());
             bookSoap.setImageUrl(b.getImageUrl());
             response.getBooks().add(bookSoap);
-        }
+        });
         return response;
     }
 }
